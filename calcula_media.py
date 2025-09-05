6a5f59e4f40766c2ad203c3f0ba29a09ab242a34
Luis <luisegrezende2005@gmail.com>
2025-09-05 19:51:56 -0300

    feito

--- calcula_media.py
+++ calcula_media.py
@@ -1,1 +1,10 @@
 # Sua solução aqui
+n1 = float(input('digite a nota 1: '))
+n2 = float(input('digite a nota 2: '))
+n3 = float(input('digite a nota 3: '))
+n4 = float(input('digite a nota 4: '))
+n5 = float(input('digite a nota 5: '))
+
+media = (n1 + n2 + n3 + n4 + n5) / 5
+
+print(f"{media:.2f}")
